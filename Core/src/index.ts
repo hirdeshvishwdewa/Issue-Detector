da568c44ece6ccf7301d832ffd0e0e439211de9d
Hirdesh Vishwdewa <hirdeshvishwdewa@gmail.com>
2025-01-01 17:49:35 +0530

    setup the core app, created few setup files like data-source, index etc.

---
+++ Core/src/index.ts
@@ -0,0 +1,10 @@
+
+import { AppDataSource } from './config/data-source';
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization:', err);
+  });
